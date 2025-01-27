7639810860112cbc43b68224b0c56e8dac014e8b
andrmoe <mail@andreasmoe.com>
2025-01-27 19:55:45 +0100

    Started project 2

---
+++ Project2/prime.py
@@ -0,0 +1,28 @@
+
+
+def exponentiate(exponent: int, base: int, p: int) -> int:
+    """
+    :param exponent Must be positive
+    :param base Base of the power.
+    :param p Multiplication modulo this number, typically a prime
+    """
+    if exponent == 0:
+        return 1
+    int_square_root = exponentiate(exponent >> 1, base, p)
+    factor = (int_square_root * int_square_root) % p
+    if exponent & 1:
+        return (factor * base) % p
+    else:
+        return factor
+
+
+def fermat_test(p: int) -> bool:
+    test_numbers = range(1, min(p, 100))
+    test_results = [exponentiate(p-1, a, p) for a in test_numbers]
+    return all([result == 1 for result in test_results])
+
+
+for i in range(1, 200):
+    if fermat_test(i):
+        print(i)
+
