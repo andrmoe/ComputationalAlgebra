dee7719d0941f906c786652612ee137dfdd53b6b
andrmoe <mail@andreasmoe.com>
2025-02-09 16:50:19 +0100

    First try on task automation, unsuccessful

---
+++ Project2/dodo.py
@@ -0,0 +1,12 @@
+from theory import generate_data_for_1a
+
+def task_compile_pdf():
+    return {'actions': [['pdflatex', '-interaction=nonstopmode', 'project2report.tex']],
+            'file_dep': ['project2report.tex', 'table.tex', 'prime.py', 'theory.py', 'test.py'],
+            'targets': ['project2report.pdf']}
+
+def generate_data():
+
+    return {'actions': [(generate_data_for_1a, [3, "py!\n"])],
+            'file_dep': ['theory.py'],
+            'targets': ['table.tex']}
--- Project2/theory.py
+++ Project2/theory.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
 
-ns = [10, 50, 100, 500, 1000]
-data = {
-    "n": ns,
-    "Candidates": [n*np.log(2) for n in ns]
-}
 
-df = pd.DataFrame(data)
-latex_table = df.to_latex(index=False, label="tab:exp_candidates")
+def generate_data_for_1a():
+    ns = [10, 50, 100, 500, 1000]
+    data = {
+        "n": ns,
+        "Candidates": [n*np.log(2) for n in ns]
+    }
 
-with open("table.tex", "w") as f:
-    f.write(latex_table)
+    df = pd.DataFrame(data)
+    latex_table = df.to_latex(index=False, label="tab:exp_candidates")
+
+    with open("table.tex", "w") as f:
+        f.write(latex_table)
