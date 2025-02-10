86f77881f3231029486ca00cef3eede206eefc09
Andreas Moe <mail@andreasmoe.com>
2025-02-10 13:57:40 +0100

    Finished 1c

--- Project2/dodo.py
+++ Project2/dodo.py
@@ -4,7 +4,7 @@ from extract_function_to_file import extract_function_to_file
 def task_compile_pdf():
     return {'actions': [['pdflatex', '-interaction=nonstopmode', '-output-directory=temp', 'project2report.tex']],
             'file_dep': ['project2report.tex', 'temp/prime_trial_table.tex', 'temp/range_size_table.tex',
-                         'prime.py', 'theory.py', 'test.py',
+                         'temp/sieve_table.tex', 'prime.py', 'theory.py', 'test.py',
                          'temp/fermat_test.py', 'temp/find_prime_generic.py', 'temp/find_prime.py',
                          'temp/trial_division_test.py', 'temp/prime_sieve.py'],
             'targets': ['temp/project2report.pdf']}
@@ -18,6 +18,11 @@ def task_generate_data_1b():
     return {'actions': [(generate_data_1b, )],
             'file_dep': ['theory.py'],
             'targets': ['temp/range_size_table.tex']}
+
+def task_generate_data_1c():
+    return {'actions': [(generate_data_1c, )],
+            'file_dep': ['theory.py'],
+            'targets': ['temp/sieve_table.tex']}
 
 def task_generate_prime_func_code():
     return {'actions': [(extract_function_to_file, [], {
--- Project2/theory.py
+++ Project2/theory.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from precomputed_numbers import small_primes
 
 ns = [10, 100, 1000, 10000]
 
@@ -8,7 +8,7 @@ ns = [10, 100, 1000, 10000]
 def generate_data_1a():
     data = {
         "n": ns,
-        "Candidates": [n*np.log(2) for n in ns]
+        "Candidates": [n * np.log(2) for n in ns]
     }
 
     df = pd.DataFrame(data)
@@ -17,8 +17,11 @@ def generate_data_1a():
     with open("temp/prime_trial_table.tex", "w") as f:
         f.write(latex_table)
 
+
+q = 0.01
+
+
 def generate_data_1b():
-    q = 0.01
     data = {
         "n": ns,
         "d": [int(np.ceil(n * np.log(2) * np.log(1 / q))) for n in ns]
@@ -28,4 +31,19 @@ def generate_data_1b():
     latex_table = df.to_latex(index=False)
 
     with open("temp/range_size_table.tex", "w") as f:
+        f.write(latex_table)
+
+
+def generate_data_1c():
+    data = {
+        "n": ns,
+    }
+    for i in [0, 1, 5, 25, 100]:
+        euler_product = np.prod([1 - 1 / p for p in small_primes[:i]])
+        data["$d_{" + str(i) + "}$"] = [int(np.ceil(n * euler_product * np.log(2) * np.log(1 / q))) for n in ns]
+
+    df = pd.DataFrame(data)
+    latex_table = df.to_latex(index=False)
+
+    with open("temp/sieve_table.tex", "w") as f:
         f.write(latex_table)
