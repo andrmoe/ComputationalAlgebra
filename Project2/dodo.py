597683845d795304e3fe8c20b6e372cc588f1d6c
Andreas Moe <mail@andreasmoe.com>
2025-02-11 17:37:52 +0100

    Table for experimental results

--- Project2/dodo.py
+++ Project2/dodo.py
@@ -1,28 +1,40 @@
 from theory import *
+from experiment import generate_experiment_table
 from extract_function_to_file import extract_function_to_file
+
 
 def task_compile_pdf():
     return {'actions': [['pdflatex', '-interaction=nonstopmode', '-output-directory=temp', 'project2report.tex']],
             'file_dep': ['project2report.tex', 'temp/prime_trial_table.tex', 'temp/range_size_table.tex',
-                         'temp/sieve_table.tex', 'prime.py', 'theory.py', 'test.py', 'experiment.py',
+                         'temp/sieve_table.tex', 'temp/experiment_table.tex', 'prime.py', 'theory.py', 'test.py', 'experiment.py',
                          'temp/fermat_test.py', 'temp/find_prime_generic.py', 'temp/find_prime.py',
                          'temp/trial_division_test.py', 'temp/prime_sieve.py'],
             'targets': ['temp/project2report.pdf']}
+
 
 def task_generate_data_1a():
     return {'actions': [(generate_data_1a, )],
             'file_dep': ['theory.py'],
             'targets': ['temp/prime_trial_table.tex']}
 
+
 def task_generate_data_1b():
     return {'actions': [(generate_data_1b, )],
             'file_dep': ['theory.py'],
             'targets': ['temp/range_size_table.tex']}
 
+
 def task_generate_data_1c():
     return {'actions': [(generate_data_1c, )],
             'file_dep': ['theory.py'],
             'targets': ['temp/sieve_table.tex']}
+
+
+def task_generate_data_2e():
+    return {'actions': [(generate_experiment_table, )],
+            'file_dep': ['experiment.py'],
+            'targets': ['temp/experiment_table.tex']}
+
 
 def task_generate_prime_func_code():
     return {'actions': [(extract_function_to_file, [], {
--- Project2/experiment.py
+++ Project2/experiment.py
@@ -2,6 +2,7 @@ from prime import *
 from typing import Callable
 from timeit import timeit
 from functools import partial
+import pandas as pd
 
 
 def generic_prime_finder_timer(binary_digits: int, range_size: int, search_func: Callable[[int, int], int]) -> float:
@@ -10,5 +11,17 @@ def generic_prime_finder_timer(binary_digits: int, range_size: int, search_func:
     return timeit(lambda: search_func(n_min, n_max), number=20)
 
 
-print(generic_prime_finder_timer(500, 200, find_prime))
-print(generic_prime_finder_timer(500, 200, find_prime_trial_division))
+def generate_experiment_table() -> bool:
+    data = {
+        "n": [10, 50, 100, 500],
+    }
+    for algorithm in [find_prime, find_prime_trial_division, find_prime_sieve]:
+        data[algorithm.__name__] = [
+            generic_prime_finder_timer(1 << n, 200, find_prime)
+            for n in data["n"]]
+    df = pd.DataFrame(data)
+    latex_table = df.to_latex(index=False)
+
+    with open("temp/experiment_table.tex", "w") as f:
+        f.write(latex_table)
+    return True
