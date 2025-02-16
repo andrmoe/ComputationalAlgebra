c481f9a5678d9193f15f72bff4d67e0ad07e4d2d
andrmoe <mail@andreasmoe.com>
2025-02-16 21:40:46 +0100

    Started on exhaustive search

---
+++ Project3/lattice_search.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def search_bounds(i: int, a: np.ndarray[np.int64], R: np.ndarray[np.float64], A: float) -> np.ndarray:
+    abs_Rii = abs(R[i,i])
+    if i == a.shape[0] - 1:
+        return np.array((0.0, A/abs_Rii))
+    Ai = np.sqrt(A**2 - np.sum(R[i+1:,i+1:].dot(a[i+1:]) ** 2))
+    Mi = np.sign(R[i,i])*R[i, i+1:].dot(a[i+1:])
+
+    return np.array((-Mi - Ai, -Mi + Ai))/abs_Rii
+
+
+def search_smallest_vector(basis: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    # Q is orthonormal and R is upper-triangular
+    Q, R = np.linalg.qr(basis)
+    base_A = np.linalg.norm(basis[0])
+    a = np.zeros(basis.shape[1])
+    print(basis)
+    print(R)
+    print(base_A)
+    bounds = search_bounds(basis.shape[1]-1, a, R, base_A)
+    print(bounds)
+    a[-1] = np.floor(bounds[1])
+    print(a)
+    print(search_bounds(basis.shape[1] - 2, a, R, base_A))
+
+search_smallest_vector(np.random.random((4, 4)))
