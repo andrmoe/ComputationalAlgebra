d4dc3b20142cbbf76548f52f9eebf0a9e800a250
andrmoe <mail@andreasmoe.com>
2025-02-20 19:57:23 +0100

    Enumeration for shortest vector probably working

--- Project3/lattice_search.py
+++ Project3/lattice_search.py
@@ -3,34 +3,54 @@ import numpy as np
 
 def gram_schmidt_basis(basis: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     q, r = np.linalg.qr(basis)
-    # Applying diag twice results in a matrix with only the diagonal elements from r
-    gs_basis = np.diag(np.diag(r)).dot(q)
-    gs_r = r.dot(np.diag(1 / np.diag(r)))
-    return gs_basis, gs_r
+    adjustment = np.diag(np.diag(r))
+    inverse_adjustment = np.diag(1 / np.diag(r))
+    return q.dot(adjustment), inverse_adjustment.dot(r)
 
 
-def search_bounds(i: int, a: np.ndarray[np.int64], R: np.ndarray[np.float64], A: float) -> np.ndarray:
-    abs_Rii = abs(R[i, i])
+def search_bounds(i: int, a: np.ndarray, bstar: np.ndarray, r: np.ndarray[np.float64], A: float) -> np.ndarray:
     if i == a.shape[0] - 1:
-        return np.array((0.0, A / abs_Rii))
-    Ai = np.sqrt(A ** 2 - np.sum(R[i + 1:, i + 1:].dot(a[i + 1:]) ** 2))
-    Mi = np.sign(R[i, i]) * R[i, i + 1:].dot(a[i + 1:])
+        return np.array((0.0, A))
+    Ai = np.sqrt(A ** 2 - np.sum(r[i + 1:, i + 1:].dot(a[i + 1:]) ** 2)) / np.linalg.norm(bstar.T[i])
+    Mi = r[i, i + 1:].dot(a[i + 1:])
 
-    return np.array((-Mi - Ai, -Mi + Ai)) / abs_Rii
+    return np.array((-Mi - Ai, -Mi + Ai))
 
 
 def search_smallest_vector(basis: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    # Q is orthonormal and R is upper-triangular
-    Q, R = np.linalg.qr(basis)
-    base_A = np.linalg.norm(basis[0])
-    a = np.zeros(basis.shape[1])
-    print(basis)
-    print(R)
-    print(base_A)
-    bounds = search_bounds(basis.shape[1] - 1, a, R, base_A)
-    print(bounds)
-    a[-1] = np.floor(bounds[1])
-    print(a)
-    print(search_bounds(basis.shape[1] - 2, a, R, base_A))
+    # Finding the smallest vector, conditional on the coefficients after i being fixed
+    def sub_search(i: int, a: np.ndarray, b: np.ndarray, bstar: np.ndarray, r: np.ndarray, A: float
+                   ) -> tuple[float | None, np.ndarray]:
+        bounds = search_bounds(i, a, bstar, r, A)
+        shortest_vectors: list[tuple[float, np.ndarray]] = []
 
-# search_smallest_vector(np.random.random((4, 4)))
+        current_coefficient = np.ceil(bounds[0])
+        while current_coefficient < bounds[1]:
+            next_a = np.copy(a)
+            next_a[i] = current_coefficient
+            if i == 0:
+                if np.any(next_a != 0):
+                    shortest_vectors.append((np.linalg.norm(b.dot(next_a)), next_a))
+                current_coefficient += 1
+                continue
+            length, full_a = sub_search(i-1, next_a, b, bstar, r, A)
+            if length is not None and length != 0:
+                shortest_vectors.append((length, full_a))
+            current_coefficient += 1
+        if shortest_vectors:
+            return min(shortest_vectors, key=lambda x: x[0])
+        else:
+            return None, np.array([])
+
+    # bstar is orthogonal and r is upper-triangular
+    bstar, r = gram_schmidt_basis(basis)
+    base_A = min([np.linalg.norm(b) for b in basis.T])
+    a = np.zeros(basis.shape[1])
+
+    shortest_length, shortest_coefficients = sub_search(basis.shape[0]-1, a, basis, bstar, r, base_A)
+    return basis.dot(shortest_coefficients), shortest_coefficients
+
+# basis = np.array([[8, 0, 1],
+#                   [8, 5, 4],
+#                   [3, 2, 0]])
+# print(search_smallest_vector(basis))
--- Project3/test.py
+++ Project3/test.py
@@ -5,13 +5,14 @@ import numpy as np
 def test_gram_schmidt_basis():
     basis = np.array([[0.0090681 , 0.56232371, 0.52668737],
                       [0.93955373, 0.50710907, 0.28597483],
-                      [0.11046587, 0.98436934, 0.5752546 ],
-                      [0.80575713, 0.51523256, 0.74138081]])
-    b, r = np.linalg.qr(basis)
-    for u in b:
-        for v in b:
+                      [0.11046587, 0.98436934, 0.5752546 ]])
+    b, r = gram_schmidt_basis(basis)
+    assert np.all(np.abs(b.dot(r) - basis) < 1e-15)
+    assert np.all(np.diag(r) == 1)
+    for u in b.T:
+        for v in b.T:
             if not np.all(u == v):
-                assert abs(np.dot(u, v)) < 1e-15
-    assert np.all(b.dot(r) == basis)
+                assert abs(np.dot(u, v)/(np.linalg.norm(u)*np.linalg.norm(v))) < 1e-15
+
 
 
