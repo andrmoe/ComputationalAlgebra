c8c1aa3c4f4afc49cb10597c26ce01ebd037f46a
andrmoe <mail@andreasmoe.com>
2025-03-03 22:05:57 +0100

    Reduced file size for github

--- Project3/lattice_examples.py
+++ Project3/lattice_examples.py
@@ -1,15 +1,29 @@
 import numpy as np
 from gram_schmidt import orthogonality_defect
+import pickle
 
 
 lattice_examples = [
 
 ]
 
-def generate_example(dim: int, max_entry: int = 1000, tries=10000):
+def generate_example(dim: int, max_entry: int = 1000, tries=1000):
     worst_basis = np.eye(dim)
     for _ in range(tries):
         ex = np.random.randint(low=-10*max_entry, high=10*max_entry, size=(dim, dim))/10
         if orthogonality_defect(ex) > orthogonality_defect(worst_basis):
             worst_basis = ex
     return worst_basis
+
+
+def gen_and_save_examples():
+    examples = []
+    for dim in range(50, 801, 50):
+        print(dim)
+        examples.append(generate_example(dim))
+    with open('lattices.pkl', 'wb') as f:
+        pickle.dump(examples, f)
+
+
+if __name__ == '__main__':
+    gen_and_save_examples()
